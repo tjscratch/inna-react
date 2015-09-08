a0c577c37eb93ccd2745249ede0d021f821c33ea
Alexander Sveshnikov <diamondliquid@gmail.com>
2015-09-08 18:05:10 +0300

    добавил gzip в node.js

--- src/server.js
+++ src/server.js
@@ -5,12 +5,20 @@ import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 import express from 'express';
+import compression from 'compression';
 import ReactDOM from 'react-dom/server';
 import Router from './Router';
 
 const server = global.server = express();
 
 server.set('port', (process.env.PORT || 5000));
+
+server.use(compression({
+    filter: function (req, res) {
+        return true;
+    }
+}));
+
 server.use(express.static(path.join(__dirname, 'public')));
 
 //
@@ -27,27 +35,27 @@ const templateFile = path.join(__dirname, 'templates/index.html');
 const template = _.template(fs.readFileSync(templateFile, 'utf8'));
 
 server.get('*', async (req, res, next) => {
-  try {
-    let statusCode = 200;
-    const data = { title: '', description: '', css: '', body: '' };
-    const css = [];
-    const context = {
-      onInsertCss: value => css.push(value),
-      onSetTitle: value => data.title = value,
-      onSetMeta: (key, value) => data[key] = value,
-      onPageNotFound: () => statusCode = 404
-    };
+    try {
+        let statusCode = 200;
+        const data = {title: '', description: '', css: '', body: ''};
+        const css = [];
+        const context = {
+            onInsertCss: value => css.push(value),
+            onSetTitle: value => data.title = value,
+            onSetMeta: (key, value) => data[key] = value,
+            onPageNotFound: () => statusCode = 404
+        };
 
-    await Router.dispatch({ path: req.path, context }, (state, component) => {
-      data.body = ReactDOM.renderToString(component);
-      data.css = css.join('');
-    });
+        await Router.dispatch({path: req.path, context}, (state, component) => {
+            data.body = ReactDOM.renderToString(component);
+            data.css = css.join('');
+        });
 
-    const html = template(data);
-    res.status(statusCode).send(html);
-  } catch (err) {
-    next(err);
-  }
+        const html = template(data);
+        res.status(statusCode).send(html);
+    } catch (err) {
+        next(err);
+    }
 });
 
 //
@@ -55,9 +63,9 @@ server.get('*', async (req, res, next) => {
 // -----------------------------------------------------------------------------
 
 server.listen(server.get('port'), () => {
-  if (process.send) {
-    process.send('online');
-  } else {
-    console.log('The server is running at http://localhost:' + server.get('port'));
-  }
+    if (process.send) {
+        process.send('online');
+    } else {
+        console.log('The server is running at http://localhost:' + server.get('port'));
+    }
 });
