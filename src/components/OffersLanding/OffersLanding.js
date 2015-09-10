8049b3869ddc0ff26c79ad65d3f33285f29c0c74
Alexander Sveshnikov <diamondliquid@gmail.com>
2015-09-10 23:40:34 +0300

    адаптивная верстка слайдера оферов

--- src/components/OffersLanding/OffersLanding.js
+++ src/components/OffersLanding/OffersLanding.js
@@ -11,74 +11,71 @@ import withStyles from '../../decorators/withStyles';
                     Перелет + Отель — новая альтернатива классическим турам
                 </div>
                 <div className="b-offers-landing__content">
-                    <div className="b-landing-list container">
-                        <div className="row">
-                            <div className="b-landing-list__item col-sm-6">
-                                <div className="b-landing-item media">
-                                    <div className="b-landing-item__img media-left media-middle">
-                                        <img alt="img" src={require('./img/1.png')}/>
-                                    </div>
-                                    <div className="b-landing-item__text first media-body media-middle">
-                                        <b>Cвобода выбора.</b> 200 стран, 500+ авиакомпаний, 389 тыс. отелей с
-                                        моментальным подтверждением.
-                                    </div>
+                    <div className="b-landing-list">
+                        <div className="b-landing-list__item">
+                            <div className="b-landing-item media">
+                                <div className="b-landing-item__img media-left media-middle">
+                                    <img alt="img" src={require('./img/1.png')}/>
+                                </div>
+                                <div className="b-landing-item__text first media-body media-middle">
+                                    <b>Cвобода выбора.</b> 200 стран, 500+ авиакомпаний, 389 тыс. отелей с
+                                    моментальным подтверждением.
                                 </div>
                             </div>
-                            <div className="b-landing-list__item col-sm-6">
-                                <div className="b-landing-item media">
-                                    <div className="b-landing-item__img media-left media-middle">
-                                        <img alt="img" src={require('./img/4.png')}/>
-                                    </div>
-                                    <div className="b-landing-item__text media-body media-middle">
-                                        <b>Экономия до 20%.</b> Путешествуйте по специальным "пакетным" тарифам от
-                                        лучших мировых поставщиков.
-                                    </div>
+                        </div>
+                        <div className="b-landing-list__item">
+                            <div className="b-landing-item media">
+                                <div className="b-landing-item__img media-left media-middle">
+                                    <img alt="img" src={require('./img/4.png')}/>
+                                </div>
+                                <div className="b-landing-item__text media-body media-middle">
+                                    <b>Экономия до 20%.</b> Путешествуйте по специальным "пакетным" тарифам от
+                                    лучших мировых поставщиков.
                                 </div>
                             </div>
                         </div>
 
-                        <div className="row">
-                            <div className="b-landing-list__item col-sm-6">
-                                <div className="b-landing-item media">
-                                    <div className="b-landing-item__img media-left media-middle">
-                                        <img alt="img" src={require('./img/2.png')}/>
-                                    </div>
-                                    <div className="b-landing-item__text media-body media-middle">
-                                        <b>Только регулярные рейсы.</b> Вылет в любой день недели, поездки на уикенды и праздники.
-                                    </div>
+                        <div className="b-landing-list__item">
+                            <div className="b-landing-item media">
+                                <div className="b-landing-item__img media-left media-middle">
+                                    <img alt="img" src={require('./img/2.png')}/>
+                                </div>
+                                <div className="b-landing-item__text media-body media-middle">
+                                    <b>Только регулярные рейсы.</b> Вылет в любой день недели, поездки на уикенды и
+                                    праздники.
                                 </div>
                             </div>
-                            <div className="b-landing-list__item col-sm-6">
-                                <div className="b-landing-item media">
-                                    <div className="b-landing-item__img media-left media-middle">
-                                        <img alt="img" src={require('./img/5.png')}/>
-                                    </div>
-                                    <div className="b-landing-item__text media-body media-middle">
-                                        <b>Это надежно.</b> Моментальная оплата поставщикам при бронировании.
-                                    </div>
+                        </div>
+                        <div className="b-landing-list__item">
+                            <div className="b-landing-item media">
+                                <div className="b-landing-item__img media-left media-middle">
+                                    <img alt="img" src={require('./img/5.png')}/>
+                                </div>
+                                <div className="b-landing-item__text media-body media-middle">
+                                    <b>Это надежно.</b> Моментальная оплата поставщикам при бронировании.
                                 </div>
                             </div>
                         </div>
 
-                        <div className="row">
-                            <div className="b-landing-list__item col-sm-6">
-                                <div className="b-landing-item media">
-                                    <div className="b-landing-item__img media-left media-middle">
-                                        <img alt="img" src={require('./img/3.png')}/>
-                                    </div>
-                                    <div className="b-landing-item__text media-body media-middle">
-                                        <b>Онлайн оплата банковской картой.</b> После оплаты мы пришлем ваш билет и ваучер на электронную почту.
-                                    </div>
+                        <div className="b-landing-list__item">
+                            <div className="b-landing-item media">
+                                <div className="b-landing-item__img media-left media-middle">
+                                    <img alt="img" src={require('./img/3.png')}/>
+                                </div>
+                                <div className="b-landing-item__text media-body media-middle">
+                                    <b>Онлайн оплата банковской картой.</b> После оплаты мы пришлем ваш билет и ваучер
+                                    на электронную почту.
                                 </div>
                             </div>
-                            <div className="b-landing-list__item col-sm-6">
-                                <div className="b-landing-item media">
-                                    <div className="b-landing-item__img media-left media-middle">
-                                        <img alt="img" src={require('./img/6.png')}/>
-                                    </div>
-                                    <div className="b-landing-item__text media-body media-middle">
-                                        <b>Поддержка 24x7.</b> Поможем оформить визы, страховки, закажем трансфер или экскурсию.
-                                    </div>
+                        </div>
+                        <div className="b-landing-list__item">
+                            <div className="b-landing-item media">
+                                <div className="b-landing-item__img media-left media-middle">
+                                    <img alt="img" src={require('./img/6.png')}/>
+                                </div>
+                                <div className="b-landing-item__text media-body media-middle">
+                                    <b>Поддержка 24x7.</b> Поможем оформить визы, страховки, закажем трансфер или
+                                    экскурсию.
                                 </div>
                             </div>
                         </div>
