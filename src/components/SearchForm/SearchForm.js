a235604dffc5dc53228fe515c632626fcd89427b
Maxim Stolbukhin <maxstbn@gmail.com>
2015-09-14 19:14:21 +0300

    search form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import styles from './SearchForm.scss';
 import withStyles from '../../decorators/withStyles';
+import TabsNav from '../TabsNav';
 
 @withStyles(styles) class SearchForm {
 
@@ -9,18 +10,39 @@ import withStyles from '../../decorators/withStyles';
         return (
             <section className="b-search-form">
                 <div className="b-search-form__tabs">
-                    <div className="b-tabs">
-                        <a href="#" className="b-tabs__item b-tabs__item_active">Перелет + отель</a>
-                        <a href="#" className="b-tabs__item">Авиабилеты</a>
-                    </div>
+                    <TabsNav/>
                 </div>
-                <div className="b-search-form__actions">
-                    <div className="b-search-form-action__location-from">
-                        <div className="b-suggest">
+                <div className="b-search-form__form">
+                    <div className="b-search-form__actions">
+                        <div className="b-search-form-action__location-from">
+                            <div className="b-suggest">
+                                Москва
+                            </div>
                         </div>
-                    </div>
-                    <div className="b-search-form-action__location-to">
-                        <div className="b-suggest">
+                        <div className="b-search-form-action__location-to">
+                            <div className="b-suggest">
+                                Берлин
+                            </div>
+                        </div>
+                        <div className="b-search-form-action__date-from">
+                            <div className="b-suggest">
+                                Туда
+                            </div>
+                        </div>
+                        <div className="b-search-form-action__date-to">
+                            <div className="b-suggest">
+                                Обратно
+                            </div>
+                        </div>
+                        <div className="b-search-form-action__people">
+                            <div className="b-suggest">
+                                2 человека, <span>эконом</span>
+                            </div>
+                        </div>
+                        <div className="b-search-form-action__btn">
+                            <span className="btn btn-green">
+                                Найти
+                            </span>
                         </div>
                     </div>
                 </div>
