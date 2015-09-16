584ea7d97ac7b78db3f67ea01467a584e273a01c
Alexander Sveshnikov <diamondliquid@gmail.com>
2015-09-16 12:11:43 +0300

    overlay в стиле ES6 со стилями

--- src/components/ui/Overlay/Overlay.js
+++ src/components/ui/Overlay/Overlay.js
@@ -1,86 +1,74 @@
 'use strict';
 
-import React from 'react';
+import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment';
+import withStyles from '../../../decorators/withStyles';
 
-var PropTypes = React.PropTypes;
+import styles from './Overlay.scss';
 
-var Overlay = null;
-if (canUseDOM) {
-    Overlay = React.createClass({
-        displayName: 'Overlay',
+@withStyles(styles) class Overlay extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-        propTypes: {
-            container: PropTypes.object,
-            elementType: PropTypes.string
-        },
+    static propTypes = {
+        container: PropTypes.object,
+        elementType: PropTypes.string
+    };
 
-        getDefaultProps: function getDefaultProps() {
-            return {
-                container: document.body,
-                elementType: 'div'
-            };
-        },
+    static defaultProps = {
+        container: canUseDOM ? document.body : null,
+        elementType: 'div'
+    };
 
-        shouldComponentUpdate: function shouldComponentUpdate() {
-            return false;
-        },
+    shouldComponentUpdate() {
+        return false;
+    }
 
-        componentDidMount: function componentDidMount() {
-            this.buildContainer();
-            this.renderOverlay();
-        },
+    componentDidMount() {
+        this.buildContainer();
+        this.renderOverlay();
+    }
 
-        componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
-            if (this.props.container !== nextProps.container) {
-                this.removeContainer();
-                this.buildContainer(nextProps);
-            }
-            this.renderOverlay(nextProps);
-        },
-
-        componentWillUnmount: function componentWillUnmount() {
+    componentWillReceiveProps(nextProps) {
+        if (this.props.container !== nextProps.container) {
             this.removeContainer();
-        },
-
-        buildContainer: function buildContainer(props) {
-            var thisProps = props || this.props;
-            this._overlayContainer = document.createElement(thisProps.elementType);
-            this._overlayContainer.className = 'b-overlay';
-            var container = thisProps.container;
-            container.className += ' overlay-scroll-fix';
-            console.log('overlay attach');
-            container.appendChild(this._overlayContainer);
-        },
-
-        removeContainer: function removeContainer() {
-            ReactDOM.unmountComponentAtNode(this._overlayContainer);
-            this.props.container.removeChild(this._overlayContainer);
-            this.props.container.className = this.props.container.className.replace(' overlay-scroll-fix', '');
-            console.log('overlay detach');
-            this._overlayContainer = null;
-        },
-
-        renderOverlay: function renderOverlay(props) {
-            var thisProps = props || this.props;
-            ReactDOM.render(React.Children.only(thisProps.children), this._overlayContainer);
-        },
-
-        render: function render() {
-            return null;
+            this.buildContainer(nextProps);
         }
-    });
-}
-else {
-    Overlay = React.createClass({
-        displayName: 'Overlay',
+        this.renderOverlay(nextProps);
+    }
 
-        render: function render() {
-            return null;
-        }
-    });
-}
+    componentWillUnmount() {
+        this.removeContainer();
+    }
 
+    buildContainer(props) {
+        var thisProps = props || this.props;
+        this._overlayContainer = document.createElement(thisProps.elementType);
+        this._overlayContainer.className = 'b-overlay';
+        var container = thisProps.container;
+        container.className += ' overlay-scroll-fix';
+        console.log('overlay attach');
+        container.appendChild(this._overlayContainer);
+    }
+
+    removeContainer() {
+        ReactDOM.unmountComponentAtNode(this._overlayContainer);
+        this.props.container.removeChild(this._overlayContainer);
+        this.props.container.className = this.props.container.className.replace(' overlay-scroll-fix', '');
+        console.log('overlay detach');
+        this._overlayContainer = null;
+    }
+
+    renderOverlay(props) {
+        var thisProps = props || this.props;
+        ReactDOM.render(React.Children.only(thisProps.children), this._overlayContainer);
+    }
+
+    render() {
+        return null;
+    }
+}
 
 export default Overlay;
