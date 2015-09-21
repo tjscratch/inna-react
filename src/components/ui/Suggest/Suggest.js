ffa4589a1616c2783202af070e16097fe6fd1db2
Maxim Stolbukhin <maxstbn@gmail.com>
2015-09-21 16:19:16 +0300

    разработка компонента автокомплита

--- src/components/ui/Suggest/Suggest.js
+++ src/components/ui/Suggest/Suggest.js
@@ -7,42 +7,22 @@ import styles from './Suggest.scss';
 import withStyles from '../../../decorators/withStyles';
 
 import Overlay from '../../ui/Overlay';
-import SuggestOptions from './SuggestOptions';
 import SuggestModel from './SuggestModel';
 
 @withViewport
 @withStyles(styles) class Suggest extends React.Component {
 
-    
+
     constructor(props) {
         super(props);
         this.state = {
             value: null,
+            options: null,
             showSuggest: false,
-            optionSelected: 0,
-            suggestListData: null
+            optionCounter: 0,
         };
     }
 
-    onSetLocation(item) {
-        this.onSetResult(item);
-        this.setState({
-            value: item.Name
-        })
-    }
-
-    onSetResult(data){
-        if (this.props.setResult) {
-            this.props.setResult(data)
-        }
-    }
-
-    onKeyDown(event) {
-        //console.log(event)
-        //console.log(event.keyCode)
-        this.refs.suggestOptions.keyboardSelected(2);
-    }
-    
     handleFocus(event) {
         this.setState({
             showSuggest: true
@@ -61,10 +41,9 @@ import SuggestModel from './SuggestModel';
         });
         if (event.target.value) {
             SuggestModel.getSuggest(event.target.value)
-                .then((data)=>{
-                    this.refs.suggestOptions.setData(data);
+                .then((data)=> {
                     this.setState({
-                        suggestListData: data,
+                        options: SuggestModel.focusOptionItem(data, 0),
                         showSuggest: true
                     })
                 })
@@ -75,26 +54,90 @@ import SuggestModel from './SuggestModel';
         }
     }
 
+    onSetResult(data) {
+        if (this.props.setResult) {
+            this.props.setResult(data)
+        }
+    }
+
+    handleOptionClick(item) {
+        this.selectedOption(item);
+    }
+
+    onKeyDown(event) {
+        let options = this.state.options;
+        if (event.keyCode == 13) {
+            var item = this.state.options[this.state.optionCounter];
+            this.selectedOption(item);
+        } else {
+            let counter = SuggestModel.selectedKeyDown(this.state.optionCounter, options, event.keyCode);
+            this.setState({
+                options: SuggestModel.focusOptionItem(options, counter),
+                optionCounter: counter
+            });
+        }
+    }
+
+    selectedOption(item) {
+        this.onSetResult(item);
+        this.setState({
+            value: item.Name,
+            showSuggest: false
+        });
+    }
+
+
+    renderSuggestInput() {
+        return (
+            <input className={`b-suggest__input ${this.state.showSuggest ? "b-suggest__input_focus" : ""}`}
+                   type="text"
+                   placeholder="suggest"
+                   onFocus={this.handleFocus.bind(this)}
+                   onBlur={this.handleBlur.bind(this)}
+                   onChange={this.handleChange.bind(this)}
+                   onKeyDown={this.onKeyDown.bind(this)}
+                   value={this.state.value}
+                />
+        );
+    }
+
+    renderSuggestOptions() {
+        return (
+            <ul className="b-suggest-list">
+                {this.state.options.map((item, index)=> {
+                    return (
+                        <li className={`b-suggest-item ${item.selected ? 'b-suggest-item_selected' : ''}`}
+                            key={index}
+                            onClick={this.handleOptionClick.bind(this, item)}
+                            >
+                            <span className="b-suggest-item__city-name">{item.Name}</span>,
+                            <span className="b-suggest-item__country-name">{item.CountryName}</span>
+                            <span className="b-suggest-item__iata">{item.CodeIata}</span>
+                        </li>
+                    );
+                }, this)}
+            </ul>
+        );
+    }
+
+
     renderSuggest() {
         if (this.state.showSuggest) {
             if (this.props.viewport.width < 1100) {
                 return (
                     <Overlay>
-                        <input className="b-suggest__input"
-                               type="text"
-                               placeholder="suggest"
-                               onFocus={this.handleFocus.bind(this)}
-                               onBlur={this.handleBlur.bind(this)}
-                               onChange={this.handleChange.bind(this)}
-                               value={this.state.value}
-                            />
-                        <SuggestOptions ref="suggestOptions" data={this.state.suggestListData}/>
+                        <div className="b-suggest__options">
+                            {this.renderSuggestInput()}
+                            {this.renderSuggestOptions()}
+                        </div>
                     </Overlay>
-                )
+                );
             } else {
                 return (
-                    <SuggestOptions ref="suggestOptions" setLocation={this.onSetLocation.bind(this)} data={this.state.suggestListData}/>
-                )
+                    <div className="b-suggest__options">
+                        {this.renderSuggestOptions()}
+                    </div>
+                );
             }
         }
         return null;
@@ -102,19 +145,9 @@ import SuggestModel from './SuggestModel';
 
     render() {
         return (
-            <div className="b-suggest-container"
-                 
-                >
+            <div className="b-suggest-container">
                 <div className="b-suggest">
-                    <input className="b-suggest__input" 
-                           type="text" 
-                           placeholder="suggest"
-                           onFocus={this.handleFocus.bind(this)}
-                           onBlur={this.handleBlur.bind(this)}
-                           onChange={this.handleChange.bind(this)}
-                           onKeyDown={this.onKeyDown.bind(this)}
-                           value={this.state.value}
-                        />
+                    {this.renderSuggestInput()}
                     {this.renderSuggest()}
                 </div>
             </div>
