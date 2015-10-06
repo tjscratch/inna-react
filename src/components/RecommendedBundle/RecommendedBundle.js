ef1c54456d2ff3e6db2e665cd512c484811797fd
Alexander Sveshnikov <diamondliquid@gmail.com>
2015-10-06 14:25:32 +0300

    ДП моб. кнопки рекомендованный и самый дешевый

--- src/components/RecommendedBundle/RecommendedBundle.js
+++ src/components/RecommendedBundle/RecommendedBundle.js
@@ -11,15 +11,35 @@ import PriceCard from '../PriceCard';
         super(props);
     }
 
+    recommendedClick() {
+        alert('recommendedClick');
+    }
+
+    cheapestClick() {
+        alert('cheapestClick');
+    }
+
     render() {
         //console.log('this.props.data', this.props.data);
         var events = this.props.events;
         var aviaData = this.props.data ? this.props.data.AviaInfo : null;
         var hotelData = this.props.data ? this.props.data.Hotel : null;
-        var priceData = this.props.data ? {price: this.props.data.Price } : null;
+        var priceData = this.props.data ? {price: this.props.data.Price} : null;
 
         return (
             <div className="b-recommended-bundle">
+                <div className="b-recommended-bundle__mobile-recommend-block">
+                    <div className="b-mobile-recommended-btn-block">
+                        <div className="b-mobile-recommended-btn b-mobile-recommended-btn-active"
+                            onClick={this.recommendedClick}>
+                            Рекомендованный
+                        </div>
+                        <div className="b-mobile-recommended-btn"
+                            onClick={this.cheapestClick}>
+                            Самый дешевый
+                        </div>
+                    </div>
+                </div>
                 <div className="b-recommended-bundle__title">
                     Выбранный вариант
                 </div>
@@ -29,13 +49,13 @@ import PriceCard from '../PriceCard';
                 <div className="b-recommended-bundle__content">
                     <div className="b-bundle-content">
                         <div className={`b-bundle-content__avia ${aviaData ? '' : 'g-invisible'}`}>
-                            <AviaCard events={events} data={aviaData} />
+                            <AviaCard events={events} data={aviaData}/>
                         </div>
                         <div className="b-bundle-content__dp">
-                            <HotelCard events={events} data={hotelData} />
+                            <HotelCard events={events} data={hotelData}/>
                         </div>
                         <div className="b-bundle-content__price">
-                            <PriceCard data={priceData} />
+                            <PriceCard data={priceData}/>
                         </div>
                     </div>
                 </div>
