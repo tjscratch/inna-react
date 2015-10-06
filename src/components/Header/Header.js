3086805c2e1f37b7abe0b668fc19c96a902421f9
Alexander Sveshnikov <diamondliquid@gmail.com>
2015-10-06 12:29:40 +0300

    Сделал мобильную шапку

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ import styles from './Header.scss';
 import withStyles from '../../decorators/withStyles';
 import Link from '../Link';
 import Navigation from '../Navigation';
+import NavMobile from '../NavMobile';
 
 @withStyles(styles) class Header {
 
@@ -12,10 +13,12 @@ import Navigation from '../Navigation';
         return (
             <header className="header">
                 <div className="b-header">
+                    <NavMobile className="b-header__nav-mobile"/>
                     <div className="b-header__brand">
                         <a className="b-brand" href="/" onClick={Link.handleClick}>
                             <img className="b-brand__img" src={require('./logo-small.png')} width="100" height="39" alt="Инна-Тур"/>
                             <img className="b-brand__text" src={require('./innatour.png')} width="135" height="19" alt="Инна-Тур"/>
+                            <img className="b-brand__img-mobile" src={require('./inna-logo.png')} width="72" height="30" alt="Инна-Тур"/>
                         </a>
                     </div>
                     <Navigation className="b-header__nav"/>
