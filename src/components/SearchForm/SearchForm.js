54d80b970fff958ccb0ac856c6bd64febc78e062
Maxim Stolbukhin <maxstbn@gmail.com>
2015-12-05 22:57:52 +0300

    offers

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -46,7 +46,8 @@ import Select from 'react-select';
                 <div className="b-search-form__form">
                     <Select
                         name="form-field-name"
-                        value="one"
+                        value=""
+                        placeholder="Выберите тип туризма или куда поехать"
                         options={this.state.options}
                         onChange={this.logChange.bind(this)}
                         />
