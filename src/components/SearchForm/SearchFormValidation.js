22dfc02f4f9c21fac87f69caa1f83f346b4f0313
Maxim Stolbukhin <maxstbn@gmail.com>
2015-12-24 18:21:33 +0300

    validation

--- src/components/SearchForm/SearchFormValidation.js
+++ src/components/SearchForm/SearchFormValidation.js
@@ -4,16 +4,40 @@ class SearchFormValidation {
         super.constructor(props);
         this.toId = {
             value: props.toId,
-            valid: false,
+            valid: true,
             error: null
         };
     }
 
     validation() {
-        console.log(this)
+        required(this.toId, "Выберите город или страну, куда планируете поехать")
+            .then((data)=> {
+                console.log('then');
+                console.log(data);
+            })
+            .catch((data)=> {
+                console.log('reject')
+                console.log(data)
+            })
         return 3
     }
 
 }
+
+
+function required(obj, textError) {
+    return new Promise((resolve, reject)=> {
+        if (obj.value) {
+            resolve(obj);
+        } else {
+            reject({
+                value: obj.value,
+                valid: false,
+                error: textError
+            });
+        }
+    });
+};
+
 
 module.exports = SearchFormValidation;
