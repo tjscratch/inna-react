a04e2b3d186409920266de04c66aee214b880833
Maxim Stolbukhin <maxstbn@gmail.com>
2016-02-04 17:11:25 +0300

    разбирательство с принципами работы redux

--- src/reducers/reducer_filters.js
+++ src/reducers/reducer_filters.js
@@ -3,9 +3,6 @@ import { LOAD_HOTEL_FILTERS } from '../constants/ActionTypes';
 export default function reducerFilters(state = null, action = null) {
     switch (action.type) {
         case LOAD_HOTEL_FILTERS:
-            console.log(LOAD_HOTEL_FILTERS);
-            console.log(state);
-            console.log(action);
             return state;
         default:
             return state;
