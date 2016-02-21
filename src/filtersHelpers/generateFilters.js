b53b6f52860b2f653a55f5108c95f91fbf5f8535
Maxim Stolbukhin <maxstbn@gmail.com>
2016-02-21 18:02:14 +0300

    отдельный класс фильтрации

--- src/filtersHelpers/generateFilters.js
+++ src/filtersHelpers/generateFilters.js
@@ -1,51 +1,56 @@
 export function generateFilters (filters) {
-  let stars = filters.Stars.List;
-  let types = filters.HotelType.List;
-  let taFactor = filters.TaFactor.List;
-  let extras = filters.Extra.List;
+    let stars = filters.Stars.List;
+    let types = filters.HotelType.List;
+    let taFactor = filters.TaFactor.List;
+    let extras = filters.Extra.List;
 
-  let generateFiltersObj = {
-    Stars: {},
-    HotelType: {},
-    TaFactor: {},
-    Extra: {}
-  };
+    let generateFiltersObj = {
+        Stars: {},
+        HotelType: {},
+        TaFactor: {},
+        Extra: {}
+    };
 
-  function generateStars (element) {
-    generateFiltersObj.Stars[element.Value] = {
-      Value: element.Value,
-      Price: element.Price,
-      Selected: false
+    function generateStars (element) {
+        generateFiltersObj.Stars[element.Value] = {
+            Value: element.Value,
+            Price: element.Price,
+            Selected: false
+        }
     }
-  }
-  stars.forEach(generateStars);
 
-  function generateHotelType (element) {
-    generateFiltersObj.HotelType[element.Value] = {
-      Value: element.Value,
-      Price: element.Price,
-      Selected: false
+    stars.forEach(generateStars);
+
+    function generateHotelType (element) {
+        generateFiltersObj.HotelType[element.Value] = {
+            Value: element.Value,
+            Price: element.Price,
+            Selected: false
+        }
     }
-  }
-  types.forEach(generateHotelType);
 
-  function generateTaFactor (element) {
-    generateFiltersObj.TaFactor[element.Value] = {
-      Value: element.Value,
-      Price: element.Price,
-      Selected: false
+    types.forEach(generateHotelType);
+
+    function generateTaFactor (element) {
+        generateFiltersObj.TaFactor[element.Value] = {
+            Value: element.Value,
+            Price: element.Price,
+            Selected: false
+        }
     }
-  }
-  taFactor.forEach(generateTaFactor);
 
-  function generateExtra (element) {
-    generateFiltersObj.Extra[element.Value] = {
-      Value: element.Value,
-      Price: element.Price,
-      Selected: false
+    taFactor.forEach(generateTaFactor);
+
+    function generateExtra (element) {
+        generateFiltersObj.Extra[element.Value] = {
+            Name: element.Name,
+            Value: element.Value,
+            Price: element.Price,
+            Selected: false
+        }
     }
-  }
-  extras.forEach(generateExtra);
 
-  return generateFiltersObj;
+    extras.forEach(generateExtra);
+
+    return generateFiltersObj;
 }
