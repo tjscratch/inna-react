b2a311365a70d280a5ad81cd4ef8edf80ddcd609
Maxim Stolbukhin <maxstbn@gmail.com>
2016-02-29 14:30:53 +0300

    поменял даты

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ var config = {
     tripadvisorEx: "//www.tripadvisor.ru/WidgetEmbed-cdspropertydetail?display=true&partnerId=32CB556934404C699237CD7F267CF5CE&lang=ru&locationId=",
     tripadvisorOk: "//www.tripadvisor.ru/WidgetEmbed-cdspropertydetail?display=true&partnerId=0809534566654028B2A963412AD73DD3&lang=ru&locationId=",
     rosTravelApi: 'http://api.russia.travel',
-    offerUrl: 'https://inna.ru/#/packages/search/6733-{locationId}-15.02.2016-22.02.2016-0-2-'
+    offerUrl: 'https://inna.ru/#/packages/search/6733-{locationId}-15.03.2016-22.03.2016-0-2-'
 };
 
 export default config;
