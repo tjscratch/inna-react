8f66bf18231d0629a305d8a2ea4634a8fdc13d1e
Maxim Stolbukhin <maxstbn@gmail.com>
2016-04-26 16:56:29 +0300

    контентные страницы

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -17,9 +17,9 @@ import Link from '../Link';
             <div className={this.props.className} role="navigation">
                 <div className="b-nav" role="navigation">
                     <span className="b-nav__text">+7 495 742-1212</span>
-                    <a className="b-nav__link" href="/about" onClick={Link.handleClick}>Где купить</a>
-                    <a className="b-nav__link" href="/contact" onClick={Link.handleClick}>Агентствам</a>
-                    <a className="b-nav__help b-help" href="/help" title="Помощь">
+                    <a className="b-nav__link" href="/about" onClick={Link.handleClick}>О компании</a>
+                    <a className="b-nav__link" href="/contact" onClick={Link.handleClick}>Контакты</a>
+                    <a className="b-nav__help b-help" href="https://inna.ru/#/help" title="Помощь" target="_blank">
                         <i className="b-help__icon" title="Помощь" alt="Помощь"></i>
                     </a>
                 </div>
