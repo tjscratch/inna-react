87f472f5085a6a0cabd0c69c320efcf9ce01720a
maximHome <maxstbn@gmail.com>
2016-05-26 20:18:35 +0300

    сделал декоратор для скролла

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,26 +11,36 @@ import Logo from '../ui/Logo';
 @withScroll
 @withStyles(styles)
 class Header extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
   }
 
   render() {
-    console.log(this.props.viewportScroll)
+    //console.log(this.props.viewportScroll)
+    if (this.props.viewportScroll.scrollY > 300) {
+      var style = {
+        position: 'fixed',
+        width: '100%',
+        zIndex: '2000'
+      };
+    } else {
+      var style = {};
+    }
     return (
-      <header className="header">
+      <header className="header"
+              style={style}>
         <div className="b-header">
           <NavMobile className="b-header__nav-mobile"/>
           <div className="b-header__brand">
             <Logo/>
             {/**
-            <a className="b-brand" href="/" onClick={Link.handleClick}>
+             <a className="b-brand" href="/" onClick={Link.handleClick}>
 
-              <img className="b-brand__img" src={require('./logo-small.png')} width="100" height="39" alt="Инна-Тур"/>
-              <img className="b-brand__text" src={require('./innatour.png')} width="135" height="19" alt="Инна-Тур"/>
-              <img className="b-brand__img-mobile" src={require('./inna-logo.png')} width="72" height="30"
-                   alt="Инна-Тур"/>
-            </a>
+             <img className="b-brand__img" src={require('./logo-small.png')} width="100" height="39" alt="Инна-Тур"/>
+             <img className="b-brand__text" src={require('./innatour.png')} width="135" height="19" alt="Инна-Тур"/>
+             <img className="b-brand__img-mobile" src={require('./inna-logo.png')} width="72" height="30"
+             alt="Инна-Тур"/>
+             </a>
              **/}
           </div>
           <Navigation className="b-header__nav"/>
