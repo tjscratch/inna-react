8ac6e5a1437dbda0ebd61c96c200459d41b0b561
maximHome <maxstbn@gmail.com>
2016-07-05 13:23:36 +0300

    верстка

--- src/components/TicketAbout/ticketAbout.js
+++ src/components/TicketAbout/ticketAbout.js
@@ -25,6 +25,34 @@ class TicketAbout extends Component {
     return '';
   }
 
+  renderTicketAboutHead() {
+    return (
+      <div className="TicketAboutHead">
+        <div className="TicketAboutHead__Back" onClick={this.closeTicketAbout.bind(this)}>
+          <Icon name="Back" size={35} className="TicketAboutHead__Icon"/>
+        </div>
+        <div className="TicketAboutHead__Title">
+          Подробная информация по перелету
+        </div>
+        <div className="TicketAboutHead__Close" onClick={this.closeTicketAbout.bind(this)}>
+          <i className="TicketAboutHead__Icon icon-emb-cancel"></i>
+        </div>
+      </div>
+    );
+  }
+
+  renderTicketAboutFooter() {
+    return (
+      <div className="TicketAboutFooter">
+        <div>
+        </div>
+        <Btn small={true}>
+          Выбрать
+        </Btn>
+      </div>
+    );
+  }
+
   renderBodyOneWay(item, ix) {
     let inTime = apiDateToJsDate(item.InTime)
     let outTIme = apiDateToJsDate(item.OutTime)
@@ -68,80 +96,78 @@ class TicketAbout extends Component {
   }
 
 
-  renderOneWay() {
-    let data = this.props.data;
-    let etapsTo = data.EtapsTo;
-    console.log(etapsTo);
-    return (
-      <div>
-        <div className="content-ticket-about__head content-ticket-about__head_one">
-          <div className="content-head-ticket-about__row">
-            <span>{data.CityFrom}</span>
-            <span className="icon-emb-right-small" style={{fontSize: 30}}></span>>
-            <span>{data.CityTo}</span>
-          </div>
-          <div className="content-head-ticket-about__row">
-            в пути:
-            {minutesToHHMM(data.TimeTo)}, {data.ToTransferCount > 0 ?
-            `${data.ToTransferCount} ${pluralize(data.ToTransferCount, ['пересадка', 'пересадки', 'пересадок'])}` : 'без пересадок'}
-          </div>
-        </div>
-        <div className="b-ticket_about-one__content">
-          {etapsTo.map((item, ix) => {
-            return this.renderBodyOneWay(item, ix);
-          }, this)}
-        </div>
-      </div>
-    )
-  }
-
-  renderTwoWay() {
+  renderTicketAboutContent() {
     let data = this.props.data;
     let etapsTo = this.props.data.EtapsTo;
     let etapsBack = this.props.data.EtapsBack;
 
+    let twoWay = (etapsBack.length > 0) ? true : false;
+
     return (
-      <div className="container-two-ways">
-        <div className="container-two-ways__one">
-          <div className="content-ticket-about__head content-ticket-about__head_one">
-            <div className="content-head-ticket-about__row">
-              <span>{data.CityFrom}</span>
-              <Icon name="ArrowRight" size={30}/>
-              <span className="icon-emb-right-small" style={{fontSize: 30}}></span>
-              <span>{data.CityTo}</span>
+      <div className="TicketAboutContent">
+
+        <div className="TicketAboutContent_container TicketAboutContent_container__one">
+
+          <div className="TicketAboutContentHead">
+
+            <div className="TicketAboutContentHeadRow">
+              <div className="TicketAboutContentHeadRow_item">{data.CityFrom}</div>
+              <div className="TicketAboutContentHeadRow_item">
+                <Icon name="ArrowRight" size={30}/>
+              </div>
+              <div className="TicketAboutContentHeadRow_item">{data.CityTo}</div>
             </div>
-            <div className="content-head-ticket-about__row">
-              в пути:
-              {minutesToHHMM(data.TimeTo)}, {data.ToTransferCount > 0 ?
-              `${data.ToTransferCount} ${pluralize(data.ToTransferCount, ['пересадка', 'пересадки', 'пересадок'])}` : 'без пересадок'}
+
+            <div className="TicketAboutContentHeadRow TicketAboutContentHeadRow__foot">
+              в пути:&nbsp;
+              {minutesToHHMM(data.TimeTo)},&nbsp;
+              {(data.ToTransferCount > 0) ?
+                `${data.ToTransferCount}&nbsp;${pluralize(data.ToTransferCount, ['пересадка', 'пересадки', 'пересадок'])}`
+                :
+                'без пересадок'
+              }
             </div>
           </div>
+
           <div className="b-ticket_about-one__content">
             {etapsTo.map((item, ix) => {
               return this.renderBodyOneWay(item, ix);
             }, this)}
           </div>
+
         </div>
-        <div className="container-two-ways__two">
-          <div className="content-ticket-about__head content-ticket-about__head_two">
-            <div className="content-head-ticket-about__row">
-              <span>{data.CityTo}</span>
-              <span className="icon-emb-right-small" style={{fontSize: 30}}></span>
-              <span>{data.EtapsBack[data.EtapsBack.length - 1].InCity}</span>
+
+
+        {twoWay ?
+          <div className="TicketAboutContent_container TicketAboutContent_container__two">
+
+            <div className="TicketAboutContentHead">
+              <div className="TicketAboutContentHeadRow">
+                <div className="TicketAboutContentHeadRow_item">{data.CityTo}</div>
+                <div className="TicketAboutContentHeadRow_item">
+                  <Icon name="ArrowRight" size={30}/>
+                </div>
+                <div className="TicketAboutContentHeadRow_item">{data.EtapsBack[data.EtapsBack.length - 1].InCity}</div>
+              </div>
+              <div className="TicketAboutContentHeadRow TicketAboutContentHeadRow__foot">
+                в пути:&nbsp;
+                {minutesToHHMM(data.TimeBack)},&nbsp;
+                {data.BackTransferCount > 0 ?
+                  `${data.BackTransferCount}&nbsp;${pluralize(data.BackTransferCount, ['пересадка', 'пересадки', 'пересадок'])}`
+                  :
+                  'без пересадок'
+                }
+              </div>
             </div>
-            <div className="content-head-ticket-about__row">
-              в пути:
-              {minutesToHHMM(data.TimeBack)}, {data.BackTransferCount > 0 ?
-              `${data.BackTransferCount} ${pluralize(data.BackTransferCount, ['пересадка', 'пересадки', 'пересадок'])}` :
-              'без пересадок'}
+            <div className="b-ticket_about-one__content">
+              {etapsBack.map((item, ix) => {
+                return this.renderBodyOneWay(item, ix);
+              }, this)}
             </div>
           </div>
-          <div className="b-ticket_about-one__content">
-            {etapsBack.map((item, ix) => {
-              return this.renderBodyOneWay(item, ix);
-            }, this)}
-          </div>
-        </div>
+          : null}
+
+
       </div>
     )
   }
@@ -151,37 +177,15 @@ class TicketAbout extends Component {
   }
 
   render() {
-    let data = this.props.data;
     return (
       <Overlay className="Overlay__TicketAbout">
-        <div className="Overlay__TicketAboutOverlay"
-             onClick={this.closeTicketAbout.bind(this)}>
+        <div className="Overlay__TicketAboutOverlay" onClick={this.closeTicketAbout.bind(this)}>
           <div className="TicketAbout">
-
-            <div className={`b-ticket-about_one-way ${data.EtapsBack.length > 0 ? 'b-ticket-about_two-ways' : ''}`}>
-              <div className="TicketAboutHead">
-                <div className="TicketAboutHead__Back" onClick={this.closeTicketAbout.bind(this)}>
-                  <Icon name="Back" size={35} className="TicketAboutHead__Icon"/>
-                </div>
-                <div className="TicketAboutHead__Title">
-                  Подробная информация по перелету
-                </div>
-                <div className="TicketAboutHead__Close" onClick={this.closeTicketAbout.bind(this)}>
-                  <i className="TicketAboutHead__Icon icon-emb-cancel"></i>
-                </div>
-              </div>
-              <div className="b-tickets-about__content-ticket-about">
-                {(data.EtapsBack.length > 0) ? this.renderTwoWay() : null}
-              </div>
-              <div className="TicketAboutFooter">
-                <div>
-                </div>
-                <Btn small={true}>
-                  Выбрать
-                </Btn>
-              </div>
+            <div className="TicketAbout__container">
+              {this.renderTicketAboutHead()}
+              {this.renderTicketAboutContent()}
+              {this.renderTicketAboutFooter()}
             </div>
-
           </div>
         </div>
       </Overlay>
