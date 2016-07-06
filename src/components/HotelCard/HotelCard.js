8cd3c094ee2c9eb03a6e8cd0bdd0c24190ac7161
maximHome <maxstbn@gmail.com>
2016-07-06 17:28:40 +0300

    прикрутил лоадер для фотографий

--- src/components/HotelCard/HotelCard.js
+++ src/components/HotelCard/HotelCard.js
@@ -1,129 +1,144 @@
-import React, { PropTypes } from 'react';
-import styles from './HotelCard.scss';
-import withStyles from '../../decorators/withStyles';
-import withViewport from '../../decorators/withViewport';
+import React, {PropTypes} from 'react'
+import styles from './HotelCard.scss'
+import withStyles from '../../decorators/withStyles'
+import withViewport from '../../decorators/withViewport'
 
-import Tripadvisor from '../Tripadvisor';
+import Tripadvisor from '../Tripadvisor'
 
-import { apiDateToJsDate, dateToDDMMM } from '../../helpers/DateHelper.js';
-import { pluralize } from '../../helpers/CountHelper.js';
+import {apiDateToJsDate, dateToDDMMM} from '../../helpers/DateHelper.js'
+import {pluralize} from '../../helpers/CountHelper.js'
 
-import ListType from '../PackagesSearchResultsPage/ListType.js';
-import HotelStars from '../HotelStars';
+import ListType from '../PackagesSearchResultsPage/ListType.js'
+import HotelStars from '../HotelStars'
 import ButtonSecondary from '../ui/Buttons/ButtonSecondary/ButtonSecondary.js'
 
-import Location from '../../core/Location';
+import Location from '../../core/Location'
+import RemoteImage from 'react-remote-image'
+import Spinner from '../ui/Spinner'
+import Icon from '../ui/Icon'
+
 
 @withViewport
 @withStyles(styles)
 class HotelCard extends React.Component {
-    constructor (props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
+
+  getMealNameByCode(code) {
+    switch (code) {
+      case 'RO':
+        return 'Без питания';
+      case 'BB':
+        return 'Завтрак';
+      case 'HB':
+        return 'Полупансион';
+      case 'FB':
+        return 'Полный пансион';
+      case 'AI':
+        return 'Все включено';
+    }
+  }
+
+  actionClick() {
+    var {events} = this.props;
+    if (events && events.changeListType) {
+      events.changeListType(ListType.Hotels);
+    }
+  }
+
+  goHotelInfo() {
+    let url = this.props.hotelUrl;
+    console.log('go', url);
+    Location.pushState(null, url);
+  }
+
+
+  renderActions() {
+    var {data, showChangeHotels, viewport} = this.props;
+
+    if (data) {
+      return (
+        <div className="b-hotel-card__actions">
+          {
+            data.HotelsCount ?
+              <ButtonSecondary onClick={this.actionClick.bind(this)}>
+                Еще {data.HotelsCount} {pluralize(data.HotelsCount, ['отеля', 'отелей', 'отелей'])}
+              </ButtonSecondary>
+              :
+              <ButtonSecondary onClick={this.actionClick.bind(this)}>
+                Заменить отель
+              </ButtonSecondary>
+          }
+          <ButtonSecondary ButtonType='Link' onClick={this.goHotelInfo.bind(this)}>
+            Подробнее
+          </ButtonSecondary>
+        </div>
+      );
+    }
+    return null;
+  }
+
+  render() {
+    var {data, allowActions, viewport} = this.props;
+
+    if (viewport.isMobile) {
+      var style = {
+        width: viewport.width
+      };
+    } else {
+      var style = null;
     }
 
-    getMealNameByCode (code) {
-        switch (code) {
-            case 'RO':
-                return 'Без питания';
-            case 'BB':
-                return 'Завтрак';
-            case 'HB':
-                return 'Полупансион';
-            case 'FB':
-                return 'Полный пансион';
-            case 'AI':
-                return 'Все включено';
-        }
-    }
+    if (data) {
+      var checkIn = apiDateToJsDate(data.CheckIn);
+      var checkOut = apiDateToJsDate(data.CheckOut);
 
-    actionClick () {
-        var { events } = this.props;
-        if (events && events.changeListType) {
-            events.changeListType(ListType.Hotels);
-        }
-    }
-
-    goHotelInfo () {
-        let url = this.props.hotelUrl;
-        console.log('go', url);
-        Location.pushState(null, url);
-    }
-
-
-    renderActions () {
-        var { data, showChangeHotels, viewport } = this.props;
-
-        if (data) {
-            return (
-                <div className="b-hotel-card__actions">
-                    {
-                        data.HotelsCount ?
-                            <ButtonSecondary onClick={this.actionClick.bind(this)}>
-                                Еще {data.HotelsCount} {pluralize(data.HotelsCount, ['отеля', 'отелей', 'отелей'])}
-                            </ButtonSecondary>
-                            :
-                            <ButtonSecondary onClick={this.actionClick.bind(this)}>
-                                Заменить отель
-                            </ButtonSecondary>
-                    }
-                    <ButtonSecondary ButtonType='Link' onClick={this.goHotelInfo.bind(this)}>
-                        Подробнее
-                    </ButtonSecondary>
+      return (
+        <div className="b-hotel-card" style={style}>
+          <div className='HotelInfo'>
+            <div className='HotelInfoPhoto'>
+              <RemoteImage
+                src={data.HotelPhoto180}
+                renderLoading={() => (
+                  <Spinner/>
+                )}
+                renderFetched={({ src }) => (
+                  <img src={src} className="HotelInfoPhoto__Image" />
+                )}
+                renderFailure={(error, retry) => (
+                  <Icon className="HotelInfoPhoto__Fail" name="NoPhoto" size={80}/>
+                )}
+              />
+            </div>
+            <div className='HotelInfo__text'>
+              <div className='HotelInfo__text-rating'>
+                <HotelStars data={data.Stars}/>
+              </div>
+              <h2 className='HotelInfo__text-title'>
+                <div className='HotelInfo__text-title-inset'>
+                  {data.HotelName}
                 </div>
-            );
-        }
-        return null;
+              </h2>
+              <div className='HotelInfo__text-date'>
+                с {dateToDDMMM(checkIn)} по {dateToDDMMM(checkOut)},
+                {`${data.NightCount} ${pluralize(data.NightCount, ['ночь', 'ночи', 'ночей'])}`}
+              </div>
+              <div className='HotelInfo__text-meal'>
+                {this.getMealNameByCode(data.MealCode)} ({data.MealCode})
+              </div>
+              <div className='HotelInfo__text-rating-ta'>
+                <Tripadvisor data={{TaCommentCount: data.TaCommentCount, TaFactor: data.TaFactor}}/>
+              </div>
+            </div>
+          </div>
+          {allowActions ? this.renderActions() : null}
+        </div>
+      );
     }
 
-    render () {
-        var { data, allowActions, viewport } = this.props;
-
-        if(viewport.isMobile){
-            var style = {
-                width: viewport.width
-            };
-        }else{
-            var style = null;
-        }
-
-        if (data) {
-            var checkIn = apiDateToJsDate(data.CheckIn);
-            var checkOut = apiDateToJsDate(data.CheckOut);
-
-            return (
-                <div className="b-hotel-card" style={style}>
-                    <div className='HotelInfo'>
-                        <div className='HotelInfo__photos'>
-                            <img className="HotelInfo__photos-photo" src={data.HotelPhoto180}/>
-                        </div>
-                        <div className='HotelInfo__text'>
-                            <div className='HotelInfo__text-rating'>
-                                <HotelStars data={data.Stars}/>
-                            </div>
-                            <h2 className='HotelInfo__text-title'>
-                                <div className='HotelInfo__text-title-inset'>
-                                    {data.HotelName}
-                                </div>
-                            </h2>
-                            <div className='HotelInfo__text-date'>
-                                с {dateToDDMMM(checkIn)} по {dateToDDMMM(checkOut)},
-                                {`${data.NightCount} ${pluralize(data.NightCount, ['ночь', 'ночи', 'ночей'])}`}
-                            </div>
-                            <div className='HotelInfo__text-meal'>
-                                {this.getMealNameByCode(data.MealCode)} ({data.MealCode})
-                            </div>
-                            <div className='HotelInfo__text-rating-ta'>
-                                <Tripadvisor data={{TaCommentCount: data.TaCommentCount, TaFactor: data.TaFactor}}/>
-                            </div>
-                        </div>
-                    </div>
-                    {allowActions ? this.renderActions() : null}
-                </div>
-            );
-        }
-
-        return null;
-    }
+    return null;
+  }
 }
 
 export default HotelCard;
